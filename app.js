bbab741b156eae10060ecd151e9b573724029877
gary909 <garywhite61@hotmail.com>
2022-11-29 13:08:14 +0100

    code working

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // Create a function called args_count that returns the number of arguments provided
+function args_count() {
+    return arguments.length;
+}
 
 console.log(args_count(1, 2)); // 2
 console.log(args_count()); // 0
